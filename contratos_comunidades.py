6b4911144bf4d2644202225eed21d1c1728475b6
pabloperism <48390795+pabloperism@users.noreply.github.com>
2019-10-20 11:58:26 +0200

    Add files via upload

---
+++ contratos_comunidades.py
@@ -0,0 +1,60 @@
+import csv
+
+from dumbo import main
+
+def load_comunidades_provincias(comunidades_file):
+ 	comunidades = {}
+ 	try:
+ 		# Read table - medal|prize|country|year
+ 		with open(comunidades_file) as f:
+ 			reader = csv.reader(f, delimiter=';', quotechar='"', doublequote=False)
+			 reader.next()
+	 		for line in reader:
+	 			comunidades[line[0]] = line[1]
+	 
+	 except:
+	 	pass
+
+	 return comunidades
+
+
+class Parse_contratos_municipio_mapper:
+    def __init__(self):
+        self.provincia = load_comunidades_provincias('./Comunidades_y_provincias.csv')
+
+    def __call__(self, key, value):
+        try:
+            codigo_mes, provincia, municipio, total_contratos, contratos_hombres, contratos_mujeres = value.split(';')
+
+            if contratos_mujeres > 0 and (provincia) in self.provincia:
+                total_contratos_mujeres += int(contratos_mujeres)
+
+            if contratos_hombres > 0 and (provincia) in self.provincia:
+                total_contratos_hombres += int(contratos_hombres)
+
+            yield provincia, (contratos_mujeres, contratos_hombres)
+
+        except:
+            pass
+
+def join_comunidades_provincias_contratos_reduce(key, values):
+    acc_mujeres = 0
+    acc_hombres = 0
+
+    comunidades = key[:]
+
+    for v in values:
+        total_contratos_mujeres, total_contratos_hombres = v[:]
+        acc_mujeres += int(total_contratos_hombres)
+        acc_hombres += int(total_contratos_hombres)
+
+    yield comunidades, (acc_mujeres, acc_hombres)
+
+
+def runner(job):
+    inout_opts = [("inputformat", "text"), ("outputformat", "text")]
+    o1 = job.additer(Parse_contratos_municipio_mapper, join_comunidades_provincias_contratos_reduce, opts=inout_opts)
+
+
+if __name__ == "__main__":
+    main(runner)
