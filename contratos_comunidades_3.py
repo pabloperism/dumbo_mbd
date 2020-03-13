c25817522bffa8142346edc9fc4c95c3afc5347a
pabloperism <48390795+pabloperism@users.noreply.github.com>
2020-03-13 12:10:59 +0100

    Add files via upload

--- contratos_comunidades_3.py
+++ contratos_comunidades_3.py
@@ -1,61 +1,70 @@
-from dumbo import main, MultiMapper, primary, secondary, JoinReducer
+import csv
 
+def load_comunidades_provincias(comunidades_file):
+   comunidad_provincia = {}
+   try:
+      # Read table - comunidades|provincia
+      with open(comunidades_file) as f:
+         reader = csv.reader(f, delimiter=';', quotechar='"', doublequote=False)
+         reader.next()
+         for line in reader:
+            comunidad_provincia[line[1]] = line[0]
+   
+   except:
+      pass
 
-def parse_comunidades_provincias_map(key, value):
-    """
-    Parse table
-    """
-    try:
-        comunidad, provincia = value.split(';')
-        yield (comunidad), (provincia)
-    except:
-        pass
+   return comunidad_provincia
 
-def parse_contratos_municipios_map(key, value):
-    """
-    Parse table
-    """
-    try:
-        codigo_mes, provincia, municipio, total_contratos, contratos_hombres, contratos_mujeres = value.split(';')
-        yield (provincia), (int(contratos_mujeres),int(contratos_hombres))
-    except:
-        pass
+def Parse_contratos_municipio_mapper(key, values):
+        try:
+            codigo_mes, provincia, municipio, total_contratos, contratos_hombres, contratos_mujeres = value.split(';')
+            int(contratos_hombres)
+            int(contratos_mujeres)
 
-class Join_comunidades_contratos_reduce(JoinReducer):
+            yield (provincia, (contratos_hombres, contratos_mujeres))
+
+        except:
+            pass
+
+class join_comunidades_provincias_contratos_reduce:
+
     def __init__(self):
-        super(Join_comunidades_contratos_reduce, self).__init__()
+        self.provincia = load_comunidades_provincias('./Comunidades_y_provincias.csv')
 
-    def primary(self, key, values):
-        self.comunidades_cache = {}
-        for v in values:
-            self.comunidades_cache(key[0]) = v[1]
+    def __call__(self, key, value):
+           try:
+              total_contratos_mujeres = 0
+              total_contratos_hombres = 0
 
-    def secondary(self, key, values):
-        acc_mujeres = 0
-        acc_hombres = 0
-        for v in values:
-            contratos_mujeres, contratos_hombres = v[:]
-            
-            if contratos_mujeres > 0 and (key[1], 'contratos_mujeres') in self.comunidades_cache:
-                acc_mujeres += int(self.comunidades_cache[(key[0], 'contratos_mujeres')])
-            
-            if contratos_hombres > 0 and (key[1], 'contratos_hombres') in self.comunidades_cache:
-                acc_hombres += int(self.comunidades_cache[(key[0], 'contratos_hombres')])
+              acc_mujeres = 0
+              acc_hombres = 0
 
-        # Emit values
-        yield key, (acc_hombres, acc_mujeres)
+              comunidad = self.provincia.get(provincia)            
 
-    def secondary_blocked(self, b):
-        if self._key != b:
-            self.country_cache = {}
-        return False
+              provincia = key[:]
+
+              for v in values:
+                  total_contratos_mujeres, total_contratos_hombres = v[:]
+                  acc_mujeres += int(total_contratos_mujeres)
+                  acc_hombres += int(total_contratos_hombres)
+
+                  if contratos_mujeres > 0 and provincia in self.provincia:
+                      total_contratos_mujeres += int(contratos_mujeres)
+
+                  if contratos_hombres > 0 and provincia in self.provincia:
+                      total_contratos_hombres += int(contratos_hombres)
+
+
+              yield comunidad, (acc_mujeres, acc_hombres)
+      except:
+          pass
+
+from dumbo import main
 
 def runner(job):
     inout_opts = [("inputformat", "text"), ("outputformat", "text")]
-    multimap = MultiMapper()
-    multimap.add("comunidad", primary(parse_comunidades_provincias_map))
-    multimap.add("contratos_mujeres", secondary(parse_contratos_municipios_map))
-    o1 = job.additer(multimap, Join_comunidades_contratos_reduce, opts=inout_opts)
+    o1 = job.additer(Parse_contratos_municipio_mapper, join_comunidades_provincias_contratos_reduce, opts=inout_opts)
+
 
 if __name__ == "__main__":
     main(runner)
